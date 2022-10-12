9fe750a312b6d5753be7cb9e4a3dacf20bc632e8
SydneyLoats <sydneyloats@gmail.com>
2022-10-12 09:56:33 -0500

    comitting input changes

--- project1/project1main.py
+++ project1/project1main.py
@@ -1,10 +1,22 @@
 import numpy as np
+import sys
 
 #take in values for number of springs/masses, spring constants (c) and masses (m)
-spring_count = 3
-mass_count = 3
 
-spring_constant = [2, 2, 2]
+if len(sys.argv) == 1:
+  print(f'Please enter input values')
+  print(f'Usage:')
+  print(f'python3 project1main.py [number of springs] [number of masses]')
+  print(f'Example:')
+  print(f'python3 project1main.py 4 3')
+  exit()
+
+spring_count = int(sys.argv[1])
+mass_count = int(sys.argv[2])
+print(f'Spring count: {spring_count}')
+print(f'Mass count: {mass_count}')
+
+spring_constant = [2, 2, 2, 2]
 m_vec = [3, 3, 3]
 
 f_vec = np.multiply(m_vec, 9.8)
@@ -24,22 +36,53 @@ def create_identity_matrix(size, val):
 
 
 
-
-
-def fixed_fixed_function():
-
-#creating A matrix
-  a_mat = np.array([[0 for c in range(mass_count)] for r in range(spring_count)])
+def a_mat_fixed_fixed():
+  ret_mat = np.array([[0 for c in range(mass_count)] for r in range(spring_count)])
 
   for r in range(spring_count):
     for c in range(mass_count):
       if r == c:
-        a_mat[r][c] = 1 
-    #    print(f'r: {r} and c: {c}')
+        ret_mat[r][c] = 1 
       elif r == c+1:
-        a_mat[r][c] = -1
-    #    print(f'r: {r} and c: {c}')
+        ret_mat[r][c] = -1
+  return ret_mat
+ 
 
+
+def a_mat_fixed_free():  
+  ret_mat = np.array([[0 for c in range(mass_count)] for r in range(spring_count)])
+
+  for r in range(spring_count):
+    for c in range(mass_count):
+      if r == c:
+        ret_mat[r][c] = 1 
+      elif c == r-1:
+        ret_mat[r][c] = -1
+  return ret_mat
+
+
+
+def a_mat_free_free():
+  ret_mat = np.array([[0 for c in range(mass_count)] for r in range(spring_count)])
+
+  for r in range(spring_count):
+    for c in range(mass_count):
+      if r == c:
+        ret_mat[r][c] = -1
+      elif c == r+1:
+        ret_mat[r][c] = 1
+  return ret_mat
+
+#test_mat = a_mat_fixed_fixed()
+#print(f'a matrix fixed fixed \n{test_mat}')
+
+
+
+
+def solve(mat):
+
+#creating A matrix
+  a_mat = mat
   print(f'A matrix is \n{a_mat}')
 
 #identity matrix
@@ -70,54 +113,6 @@ def fixed_fixed_function():
   print(f'u (displacement) vector is \n{u_vec}')
 
 
+solve(a_mat_fixed_fixed())
 
 
-
-def fixed_free_function():
-
-#creating A matrix
-  a_mat = np.array([[0 for c in range(mass_count)] for r in range(spring_count)])
-
-  for r in range(spring_count):
-    for c in range(mass_count):
-      if r==c:
-        a_mat[r][c] = 1 
-      #  print(f'r: {r} and c: {c}')
-      elif c == r-1:
-        a_mat[r][c] = -1
-      #  print(f'r: {r} and c: {c}')
-  print(f'A matrix: \n{a_mat}')
-
-#identity matrix
-  i_mat = create_identity_matrix(len(spring_constant), spring_constant[0])
-  print(f'I matrix is \n{i_mat}')
-
-#A transpose
-  at_mat = a_mat.transpose()
-  print(f'A transpose matrix is \n{at_mat}')
-
-#A transpose inverse matrix
-  ati_mat = np.linalg.pinv(at_mat)
-  print(f'A transpose inverse matrix is \n{ati_mat}')
-
-#solving for w
-  w_vec = np.matmul(ati_mat, f_vec)
-  print(f'w vector is \n{w_vec}')
-
-#solving for e
-  ii_mat = np.linalg.pinv(i_mat)
-  e_vec = np.matmul(ii_mat, w_vec) 
-  print(f'e (elongation) vector is \n{e_vec}')
-
-#solving for u (displacement)
-  ai_mat = np.linalg.pinv(a_mat)
-  print(f'A inverse matrix is \n{ai_mat}')
-  u_vec = np.matmul(ai_mat, e_vec)
-  print(f'u (displacement) vector is \n{u_vec}')
-
-
-
-
-
-#fixed_fixed_function()
-fixed_free_function()
