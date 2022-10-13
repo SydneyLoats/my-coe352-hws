f20d6020647eb91c1f01a3531fe185d20d31fc45
SydneyLoats <sydneyloats@gmail.com>
2022-10-13 17:06:12 -0500

    committing with updated logic

--- project1/project1.py
+++ project1/project1.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 from scipy.linalg import svd
 
+#assign number of springs and spring constants
 global spring_count
 spring_count = int(input('Input number of springs (positive integer): '))
 if spring_count < 1:
@@ -15,6 +16,7 @@ for i in range(spring_count):
   spring_const_vec[i] = int(input(f'Input spring constant {i+1}: '))
 print(f'Spring constant vector is {spring_const_vec}')
 
+#assign number of masses and mass values
 global mass_count
 mass_count = int(input('Input number of masses (positive integer): '))
 if mass_count < 1:
@@ -28,6 +30,7 @@ for i in range(mass_count):
   m_vec[i] = int(input(f'Input mass value {i+1}: '))
 print(f'Mass values vector is {m_vec}')
 
+#determine whether to use fixed-fixed or fixed-free
 global system_type
 system_type = int(input(f'Input boundary condition(either 0 or 1 where 0=fixed-fixed and 1=fixed-free): '))
 print(f'Boundary input condition is {system_type}')
@@ -38,20 +41,20 @@ elif (spring_count - mass_count) == 0 and system_type !=1:
   print('\nError: the number of masses and springs do not match the boundary condition, please choose different numbers')
   exit()
  
-
+#create external force vector, f
 global f_vec
 f_vec = np.multiply(m_vec, 9.8)
 
-
-def create_identity_matrix(val_vec):
-  ret_mat = np.array([[0 for c in range(len(val_vec))] for r in range(len(val_vec))])
-  for r in range(len(val_vec)):
-    for c in range(len(val_vec)):
+#function to create an identity matrix, uses spring constant vector
+def create_identity_matrix():
+  ret_mat = np.array([[0 for c in range(len(spring_const_vec))] for r in range(len(spring_const_vec))])
+  for r in range(len(spring_const_vec)):
+    for c in range(len(spring_const_vec)):
       if r == c:
-        ret_mat[r][c] = val_vec[c]
+        ret_mat[r][c] = spring_const_vec[c]
   return ret_mat
 
-
+#function to create the A matrix for a fixed_fixed system
 def a_mat_fixed_fixed():
   ret_mat = np.array([[0 for c in range(mass_count)] for r in range(spring_count)])
   for r in range(spring_count):
@@ -62,7 +65,7 @@ def a_mat_fixed_fixed():
         ret_mat[r][c] = -1
   return ret_mat
 
-
+#function to create the A matrix for a fixed-free system
 def a_mat_fixed_free():  
   ret_mat = np.array([[0 for c in range(mass_count)] for r in range(spring_count)])
   for r in range(spring_count):
@@ -73,54 +76,11 @@ def a_mat_fixed_free():
         ret_mat[r][c] = -1
   return ret_mat
 
-
-def a_mat_free_free():
-  ret_mat = np.array([[0 for c in range(mass_count)] for r in range(spring_count)])
-  for r in range(spring_count):
-    for c in range(mass_count):
-      if r == c:
-        ret_mat[r][c] = -1
-      elif c == r+1:
-        ret_mat[r][c] = 1
-  return ret_mat
-
-
-#solve for displacement
+#function to solve for displacement, takes in the a matrix
 def solve_displacement(mat):
 #creating A matrix
   a_mat = mat
 #  print(f'A matrix is \n{a_mat}')
-
-#A transpose
-  at_mat = a_mat.transpose()
-#  print(f'A transpose matrix is \n{at_mat}')
-
-#A transpose inverse
-  ati_mat = np.linalg.pinv(at_mat)
-#  print(f'Inverse A transpose matrix is \n{ati_mat}')
-
-#finding w
-  w_vec = np.matmul(ati_mat, f_vec)
-  print(f'\nInternal stress vector, w, is \n{w_vec}')
-
-#identity matrix
-  c_mat = create_identity_matrix(spring_const_vec)
-#  print(f'C matrix is \n{c_mat}')
-
-#C inverse
-  ci_mat = np.linalg.pinv(c_mat)
-#  print(f'C inverse matrix is \n{ci_mat}')
-
-#finding e
-  e_vec = np.matmul(w_vec, ci_mat)
-  print(f'\nElongation vector, e, is \n{e_vec}')
-
-#a inverse
-  ai_mat = np.linalg.pinv(a_mat)
-#  print(f'a inverse is \n{ai_mat}')
-
-#finding u (displacement)
-  u_mat = np.matmul(ai_mat, e_vec)
 
   sv = solve_svd(a_mat)
   print(f'\nSingular values for A: \n{sv}')
@@ -131,14 +91,9 @@ def solve_displacement(mat):
   condition_value_a = find_largest_eigen(eigen_sv)/find_smallest_eigen(eigen_sv)
   print(f'\nCondition value for A: \n{condition_value_a}')
 
-  svc = solve_svd(c_mat)
-  print(f'\nSingular values for C: \n{svc}')
-
-  eigen_svc = solve_eigenvalues(svc)
-  print(f'\nEigenvalues for C: \n{eigen_svc}')
-  
-  condition_value_c = find_largest_eigen(eigen_svc)/find_smallest_eigen(eigen_svc)
-  print(f'\nCondition value for A: \n{condition_value_c}')
+#creating A transpose matrix
+  at_mat = a_mat.transpose()
+#  print(f'A transpose matrix is \n{at_mat}')
 
   sva = solve_svd(at_mat)
   print(f'\nSingular values for A transpose: \n{sva}')
@@ -149,10 +104,59 @@ def solve_displacement(mat):
   condition_value_a = find_largest_eigen(eigen_sva)/find_smallest_eigen(eigen_sva)
   print(f'\nCondition value for A transpose: \n{condition_value_a}')
 
-  print(f'\nu matrix is \n{u_mat}')
-  for i in range(len(u_mat)):
-    print(f'Displacement {i+1} = {u_mat[i]}')
-  return u_mat
+#creating identity matrix
+  c_mat = create_identity_matrix()
+#  print(f'C matrix is \n{c_mat}')
+
+  svc = solve_svd(c_mat)
+  print(f'\nSingular values for C: \n{svc}')
+
+  eigen_svc = solve_eigenvalues(svc)
+  print(f'\nEigenvalues for C: \n{eigen_svc}')
+  
+  condition_value_c = find_largest_eigen(eigen_svc)/find_smallest_eigen(eigen_svc)
+  print(f'\nCondition value for A: \n{condition_value_c}')
+
+#creating stiffness matrix, K
+  tempk_mat = np.matmul(at_mat, c_mat)
+  k_mat = np.matmul(tempk_mat, a_mat)
+
+#creating K inverse matrix
+  ki_mat = np.linalg.pinv(k_mat)
+
+#solving u matrix (displacement)
+  u_vec = np.matmul(ki_mat, f_vec)
+
+#creating e vector (elongation)
+  e_vec = np.matmul(a_mat, u_vec)
+  print(f'\nElongation vector, e, is \n{e_vec}')
+
+#creating w vector (internal forces)
+  w_vec = np.matmul(c_mat, e_vec)
+  print(f'\nInternal stress vector, w, is \n{w_vec}')
+
+#creating A transpose inverse matrix
+#  ati_mat = np.linalg.pinv(at_mat)
+#  print(f'Inverse A transpose matrix is \n{ati_mat}')
+
+#C inverse
+#  ci_mat = np.linalg.pinv(c_mat)
+#  print(f'C inverse matrix is \n{ci_mat}')
+
+#a inverse
+#  ai_mat = np.linalg.pinv(a_mat)
+#  print(f'a inverse is \n{ai_mat}')
+
+#finding u (displacement)
+#  u_mat = np.matmul(ai_mat, e_vec)
+
+
+
+
+  print(f'\nu vector is \n{u_vec}')
+  for i in range(len(u_vec)):
+    print(f'Displacement {i+1} = {u_vec[i]}')
+  return u_vec
 
 
 def solve_svd(mat):
